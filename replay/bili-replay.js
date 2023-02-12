0104218c5e48f861defff16ff34a9c47b691df5a
maomaorun <maomao.run@outlook.com>
2023-02-12 13:36:50 +0800

    适配NBL

--- replay/bili-replay.js
+++ replay/bili-replay.js
@@ -35,19 +35,19 @@ const archives = [
         // 扇宝
         authorIds: [30],
         url: 'https://api.bilibili.com/x/polymer/space/seasons_archives_list?mid=1682965468&season_id=1022826&sort_reverse=false&page_num=1&page_size=1',
-        mode: 1
+        mode: 2
     },
     {
         // 安可
         authorIds: [31],
         url: 'https://api.bilibili.com/x/series/archives?mid=1375400985&series_id=2924566&only_normal=true&sort=desc&pn=1&ps=1',
-        mode: 1
+        mode: 2
     },
     {
         // NB-Light
         authorIds: [39, 40, 41, 42, 43],
         url: 'https://api.bilibili.com/x/polymer/space/seasons_archives_list?mid=1548358039&season_id=1107501&sort_reverse=false&page_num=1&page_size=30',
-        mode: 2
+        mode: 3
     }
 ];
 
@@ -69,8 +69,7 @@ const archives = [
             for (let j = 0; j < authorIds.length; ++j) {
                 const authorId = authorIds[j];
                 const author = await ZimuApi.findAuthorById(authorId);
-                console.log(author);
-                console.log(`处理author(${authorId})的replay`);
+                console.log(`处理author(${authorId},${author.name})的replay`);
                 const clips = await ZimuApi.findClipsByAuthorId(authorId, 4, 1, 20);
                 if (clips.length === 0) continue;
 
@@ -80,10 +79,6 @@ const archives = [
                     const f2 = `${clip.datetime.substring(0,4)}-${parseInt(clip.datetime.substring(5,7))}-${parseInt(clip.datetime.substring(8,10))}`;
                     const f3 = `${clip.datetime.substring(0,4)}${clip.datetime.substring(5,7)}${clip.datetime.substring(8,10)}`;
                     const f4 = `${clip.datetime.substring(0,4)}/${clip.datetime.substring(5,7)}/${clip.datetime.substring(8,10)}`;
-                    console.log(f1);
-                    console.log(f2);
-                    console.log(f3);
-                    console.log(f4);
                     const srt = await ZimuApi.findSrtByClipId(clip.id);
                     if (srt.length !== 0) continue;
                     for (let m = 0; m < videos.length; ++m) {
@@ -150,53 +145,6 @@ const archives = [
                     }
                 }
             }
-
-            // if (video.title.indexOf(clip.title) === -1) {
-            //     console.log(`author(${archive.authorId})的合集列表中无匹配录播,${video.title},${clip.title}`);
-            //     continue;
-            // }
-            
-            // // 获取录播基础信息
-            // const infoUrl = `https://api.bilibili.com/x/web-interface/view?bvid=${video.bvid}`;
-            // const infoRes = await fetch(infoUrl);
-            // const infoJson = await infoRes.json();
-            // if (!infoRes.ok) {
-            //     await PushApi.push(`获取author(${archive.authorId})的录播基础信息失败(${video.bvid}, ${clip.title})`, JSON.stringify(infoJson));
-            //     continue;
-            // }
-            // // 获取字幕信息
-            // if (infoJson.data.subtitle.list.length > 0) {
-            //     console.log(`author(${archive.authorId})的录播(${clip.title},${video.bvid})找到智能字幕`);
-            //     const subtitleUrl = infoJson.data.subtitle.list[0].subtitle_url;
-            //     const subtitleRes = await fetch(subtitleUrl);
-            //     const subtitleJson = await subtitleRes.json();
-            //     if (!subtitleRes.ok) {
-            //         await PushApi.push(`获取录播字幕失败(${video.bvid}, ${clip.title})`, JSON.stringify(subtitleJson));
-            //         continue;
-            //     }
-            //     // json格式字幕转换成srt格式
-            //     let srt = '';
-            //     const subtitles = subtitleJson.body;
-            //     for (let k = 0; k < subtitles.length; ++k) {
-            //         const subtitle = subtitles[k];
-            //         const lineId = subtitle.sid;
-            //         const startTime = fromMilliseconds(subtitle.from * 1000);
-            //         const endTime = fromMilliseconds(subtitle.to * 1000);
-            //         const content = subtitle.content;
-            //         const line = `${lineId}\r\n${startTime} --> ${endTime}\r\n${content}\r\n\r\n`;
-            //         srt += line;
-            //     }
-            //     try {
-            //         await ZimuApi.insertSubtitle(clip.id, srt);
-            //         console.log(`author(${archive.authorId})写入字幕成功:${video.bvid},${clip.datetime},${video.title}`);
-            //     } catch (ex) {
-            //         console.log(`author(${archive.authorId})写入字幕失败:${video.bvid},${video.title}`);
-            //         await PushApi.push(`author(${archive.authorId})写入字幕失败:${video.bvid},${video.title}`, '');
-            //     }
-            // } else {
-            //     console.log(`author(${archive.authorId})的录播(${clip.title},${video.bvid})未找到智能字幕`);
-            //     await PushApi.push(`author(${archive.authorId})的录播(${clip.title},${video.bvid})未找到智能字幕`, '');
-            // }
         } catch (ex) {
             console.log(ex);
         }
