c58c2b3c19b6ee6ed2c12f951759b0e27ca9ec6a
maomaorun <maomao.run@outlook.com>
2023-02-14 23:36:16 +0800

    add cleaner

--- cleaner.js
+++ cleaner.js
@@ -16,6 +16,7 @@ import ZimuApi from './api/ZimuApi.js';
             if (clips.length === 0) continue;
             for (let k = 0; k < clips.length; ++k) {
                 const clip = clips[k];
+                console.log(clip);
                 try {
                     const YYYY = clip.datetime.substring(0, 4);
                     const MM = clip.datetime.substring(5, 7);
