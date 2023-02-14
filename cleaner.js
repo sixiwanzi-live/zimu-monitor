84ec08bede088099bc97fa8e57478ac164ddb4d8
maomaorun <maomao.run@outlook.com>
2023-02-14 23:31:54 +0800

    add cleaner

--- cleaner.js
+++ cleaner.js
@@ -24,9 +24,8 @@ import ZimuApi from './api/ZimuApi.js';
                     const hh = clip.datetime.substring(11, 13);
                     const mm = clip.datetime.substring(14, 16);
                     const ss = clip.datetime.substring(17, 19);
-                    const file = `/${organizationId}/${author.name}/${YYYY}-${MM}/${YYYY}${MM}${DD}-${hh}${mm}${ss}-${author.name}-${clip.title}.flv`;
-                    console.log(file);
-                    // await unlink(file);
+                    const file = `${config.live.root}/${organizationId}/${author.name}/${YYYY}-${MM}/${YYYY}${MM}${DD}-${hh}${mm}${ss}-${author.name}-${clip.title}.flv`;
+                    await unlink(file);
                     console.log(`删除成功:${file}`);
                 } catch (ex) {}
             }
