6d193236d4dfbd934712fa08dcc4947d50992337
maomaorun <maomao.run@outlook.com>
2023-02-23 09:25:33 +0800

    多任务并行

--- replay/bili-replay.js
+++ replay/bili-replay.js
@@ -53,15 +53,16 @@ const archives = [
 ];
 
 (async () => {
+    let tasks = [];
     for (let i = 0; i < archives.length; ++i) {
         const archive = archives[i];
         try {
-            // 获取B站视频源
+            // 获取指定archive的一批B站视频源
             let videos = [];
             const archiveRes = await fetch(archive.url);  // 请求合集列表
             const archiveJson = await archiveRes.json();
             if (!archiveRes.ok) {
-                await PushApi.push(`请求B站合集列表失败`, JSON.stringify(archiveJson));
+                PushApi.push(`请求B站合集列表失败`, JSON.stringify(archiveJson));
                 continue;
             }
             videos = archiveJson.data.archives;
@@ -69,107 +70,131 @@ const archives = [
             const authorIds = archive.authorIds;
             for (let j = 0; j < authorIds.length; ++j) {
                 const authorId = authorIds[j];
-                const author = await ZimuApi.findAuthorById(authorId);
-                console.log(`处理author(${authorId},${author.name})的replay`);
-                const clips = await ZimuApi.findClipsByAuthorId(authorId, 4, 1, 20);
-                if (clips.length === 0) continue;
-
-                for (let k = 0; k < clips.length; ++k) {
-                    const clip = clips[k];
-                    // clip信息更新入库写文件的时候，因为windows系统不支持*号，所以clip.title中的*换成了_
-                    // 这里去掉_以便于匹配B站源的标题
-                    const modifiedTitle = clip.title.replaceAll('_', '');
-                    const f1 = clip.datetime.substring(0, 10);
-                    const f2 = `${clip.datetime.substring(0,4)}-${parseInt(clip.datetime.substring(5,7))}-${parseInt(clip.datetime.substring(8,10))}`;
-                    const f3 = `${clip.datetime.substring(0,4)}${clip.datetime.substring(5,7)}${clip.datetime.substring(8,10)}`;
-                    const f4 = `${clip.datetime.substring(0,4)}/${clip.datetime.substring(5,7)}/${clip.datetime.substring(8,10)}`;
-                    const srt = await ZimuApi.findSrtByClipId(clip.id);
-                    if (srt.length !== 0) continue;
-                    for (let m = 0; m < videos.length; ++m) {
-                        const video = videos[m];
-                        let matched = false;
-                        if (archive.mode === 1) {
-                            const dt = `${clip.datetime.substring(0, 4)}年${parseInt(clip.datetime.substring(5, 7))}月${parseInt(clip.datetime.substring(8, 10))}日${parseInt(clip.datetime.substring(11, 13))}点场`;
-                            if (
-                                (
-                                    video.title.indexOf(modifiedTitle) !== -1 || 
-                                    video.title.indexOf(modifiedTitle.replaceAll(' ', '')) !== -1
-                                ) && video.title.indexOf(dt) !== -1) {
-                                matched = true;
-                            }
-                        } else if (archive.mode === 2) {
-                            if (
-                                (
-                                    video.title.indexOf(modifiedTitle) !== -1  ||
-                                    video.title.indexOf(modifiedTitle.replaceAll(' ', '')) !== -1
-                                ) && 
-                                (
-                                    video.title.indexOf(f1) !== -1 || 
-                                    video.title.indexOf(f2) !== -1 || 
-                                    video.title.indexOf(f3) !== -1 || 
-                                    video.title.indexOf(f4) !== -1
-                                )
-                            ) {
-                                matched = true;
-                            }
-                        } else if (archive.mode === 3) {
-                            if (
-                                (
-                                    video.title.indexOf(modifiedTitle) !== -1 || 
-                                    video.title.indexOf(modifiedTitle.replaceAll(' ', '')) !== -1
-                                ) && video.title.indexOf(author.name) !== -1 && 
-                                (
-                                    video.title.indexOf(f1) !== -1 || 
-                                    video.title.indexOf(f2) !== -1 || 
-                                    video.title.indexOf(f3) !== -1 || 
-                                    video.title.indexOf(f4) !== -1
-                                )
-                            ) {
-                                matched = true;
-                            }
+                tasks.push(new Promise(async (res, rej) => {
+                    try {
+                        const author = await ZimuApi.findAuthorById(authorId);
+                        console.log(`处理author(${authorId},${author.name})的replay`);
+                        // 查指定author是否存在直播中状态的clip
+                        const clips = await ZimuApi.findClipsByAuthorId(authorId, 4, 1, 20);
+                        if (clips.length === 0) {
+                            console.log(`author(${author.name})无未解析视频`);
+                            res();
+                            return;
                         }
-                        if (matched) {
-                            // 找到匹配的未处理字幕的clip
-                            try {
-                                const updatedClip = {
-                                    id: clip.id,
-                                    bv: video.bvid
-                                };
-                                console.log(`将更新clip:${updatedClip.id},${updatedClip.bv}`);
-                                await ZimuApi.updateClip(updatedClip);
-                                PushApi.push(`更新author(${authorId})的clip(${clip.id}),bv号为${video.bvid}`, '');
-                            } catch (ex) {
-                                console.log(ex);
-                                PushApi.push(`更新clip(${clip.title})的bv失败`, ex);
+                        for (let k = 0; k < clips.length; ++k) {
+                            const clip = clips[k];
+                            // clip信息更新入库写文件的时候，因为windows系统不支持*号，所以clip.title中的*换成了_
+                            // 这里去掉_以便于匹配B站源的标题
+                            const modifiedTitle = clip.title.replaceAll('_', '');
+                            const f1 = clip.datetime.substring(0, 10);
+                            const f2 = `${clip.datetime.substring(0,4)}-${parseInt(clip.datetime.substring(5,7))}-${parseInt(clip.datetime.substring(8,10))}`;
+                            const f3 = `${clip.datetime.substring(0,4)}${clip.datetime.substring(5,7)}${clip.datetime.substring(8,10)}`;
+                            const f4 = `${clip.datetime.substring(0,4)}/${clip.datetime.substring(5,7)}/${clip.datetime.substring(8,10)}`;
+                            // 已经包含字幕的不处理
+                            const srt = await ZimuApi.findSrtByClipId(clip.id);
+                            if (srt.length !== 0) {
                                 continue;
                             }
-                            let subtitles = await ZimuApi.findSubtitlesByBv(video.bvid);
-                            if (subtitles.length === 0) {
-                                console.log(`author(${authorId})未找到智能字幕:${video.bvid},${clip.datetime},${video.title}`);
-                                PushApi.push(`author(${authorId})未找到智能字幕:${video.bvid},${clip.datetime},${video.title}`, '');
-                                subtitles = await AsrApi.parse(video.bvid);
-                                if (subtitles.length === 0) {
-                                    console.log(`author(${clip.authorId})的录播(${video.title},${video.bvid})asr执行失败`);
-                                    PushApi.push(`author(${clip.authorId})的录播(${video.title},${video.bvid})asr执行失败`, '');
-                                    continue;
+                            for (let m = 0; m < videos.length; ++m) {
+                                const video = videos[m];
+                                // 将指定clip与所有b站video按照规则逐一对比，发现标题匹配的就执行解析
+                                let matched = false;
+                                if (archive.mode === 1) {
+                                    const dt = `${clip.datetime.substring(0, 4)}年${parseInt(clip.datetime.substring(5, 7))}月${parseInt(clip.datetime.substring(8, 10))}日${parseInt(clip.datetime.substring(11, 13))}点场`;
+                                    if (
+                                        (
+                                            video.title.indexOf(modifiedTitle) !== -1 || 
+                                            video.title.indexOf(modifiedTitle.replaceAll(' ', '')) !== -1
+                                        ) && video.title.indexOf(dt) !== -1) {
+                                        matched = true;
+                                    }
+                                } else if (archive.mode === 2) {
+                                    if (
+                                        (
+                                            video.title.indexOf(modifiedTitle) !== -1  ||
+                                            video.title.indexOf(modifiedTitle.replaceAll(' ', '')) !== -1
+                                        ) && 
+                                        (
+                                            video.title.indexOf(f1) !== -1 || 
+                                            video.title.indexOf(f2) !== -1 || 
+                                            video.title.indexOf(f3) !== -1 || 
+                                            video.title.indexOf(f4) !== -1
+                                        )
+                                    ) {
+                                        matched = true;
+                                    }
+                                } else if (archive.mode === 3) {
+                                    if (
+                                        (
+                                            video.title.indexOf(modifiedTitle) !== -1 || 
+                                            video.title.indexOf(modifiedTitle.replaceAll(' ', '')) !== -1
+                                        ) && video.title.indexOf(author.name) !== -1 && 
+                                        (
+                                            video.title.indexOf(f1) !== -1 || 
+                                            video.title.indexOf(f2) !== -1 || 
+                                            video.title.indexOf(f3) !== -1 || 
+                                            video.title.indexOf(f4) !== -1
+                                        )
+                                    ) {
+                                        matched = true;
+                                    }
                                 }
-                            };
-                            try {
-                                await ZimuApi.insertSubtitle(clip.id, subtitles);
-                                console.log(`author(${authorId})写入智能字幕成功:${video.bvid},${clip.datetime},${video.title}`);
-                                PushApi.push(`author(${authorId})写入智能字幕成功:${video.bvid},${clip.datetime},${video.title}`, '');
-                            } catch (ex) {
-                                console.log(ex);
-                                console.log(`author(${authorId})写入智能字幕失败:${video.bvid},${clip.datetime},${video.title}`);
-                                PushApi.push(`author(${authorId})写入智能字幕失败:${video.bvid},${clip.datetime},${video.title}`, '');
+                                if (matched) {
+                                    // 更新clip状态
+                                    try {
+                                        const updatedClip = {
+                                            id: clip.id,
+                                            bv: video.bvid
+                                        };
+                                        console.log(`将更新clip:${updatedClip.id},${updatedClip.bv}`);
+                                        await ZimuApi.updateClip(updatedClip);
+                                        PushApi.push(`更新author(${authorId})的clip(${clip.id}),bv号为${video.bvid}`, '');
+                                    } catch (ex) {
+                                        console.log(ex);
+                                        PushApi.push(`更新clip(${clip.title})的bv失败`, ex);
+                                        continue;
+                                    }
+                                    // 查找智能字幕，如果智能字幕不存在，则调用asr处理
+                                    let subtitles = await ZimuApi.findSubtitlesByBv(video.bvid);
+                                    if (subtitles.length === 0) {
+                                        console.log(`author(${authorId})未找到智能字幕:${video.bvid},${clip.datetime},${video.title}`);
+                                        PushApi.push(`author(${authorId})未找到智能字幕:${video.bvid},${clip.datetime},${video.title}`, '');
+                                        subtitles = await AsrApi.parse(video.bvid);
+                                        if (subtitles.length === 0) {
+                                            console.log(`author(${clip.authorId})的录播(${video.title},${video.bvid})asr执行失败`);
+                                            PushApi.push(`author(${clip.authorId})的录播(${video.title},${video.bvid})asr执行失败`, '');
+                                            continue;
+                                        } else {
+                                            console.log(`author(${clip.authorId})的录播(${video.title},${video.bvid})asr执行成功`);
+                                            PushApi.push(`author(${clip.authorId})的录播(${video.title},${video.bvid})asr执行成功`, '');
+                                        }
+                                    };
+                                    try {
+                                        await ZimuApi.insertSubtitle(clip.id, subtitles);
+                                        console.log(`author(${authorId})写入智能字幕成功:${video.bvid},${clip.datetime},${video.title}`);
+                                        PushApi.push(`author(${authorId})写入智能字幕成功:${video.bvid},${clip.datetime},${video.title}`, '');
+                                    } catch (ex) {
+                                        console.log(ex);
+                                        console.log(`author(${authorId})写入智能字幕失败:${video.bvid},${clip.datetime},${video.title}`);
+                                        PushApi.push(`author(${authorId})写入智能字幕失败:${video.bvid},${clip.datetime},${video.title}`, '');
+                                    }
+                                }
                             }
                         }
-
+                        res();
+                    } catch (ex) {
+                        console.error(ex);
+                        rej();
                     }
-                }
+                }));
             }
         } catch (ex) {
             console.log(ex);
         }
+    }
+    try {
+        await Promise.all(tasks);
+    } catch (ex) {
+        console.error(ex);
     }
 })()
